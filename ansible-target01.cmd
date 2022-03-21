68c23275c5a037bede0169218d2306c0a03d6c46
Neko1999 <43975094+Neko1999@users.noreply.github.com>
2022-03-21 11:36:49 -0500

    Update ansible-target01.cmd

--- ansible-target01.cmd
+++ ansible-target01.cmd
@@ -2,7 +2,7 @@
 d:
 cd "D:\wintools\qemu-w64-setup-20210810"
 
-set DISCO=d:/talleresteam/ansible/CentOS-7-x86_64-ansible-targe01.qcow2
+set DISCO=C:\Proyecto-Ansible\Kyndryl-Ansible\CentOS-7-x86_64-ansible-targe01-v4-ready.qcow2
 
 qemu-system-x86_64 -smp 8 -machine q35 -m 1024 -drive file=%DISCO%,media=disk,if=virtio -net nic -net user,id=net0,hostfwd=tcp::10024-:22 -netdev socket,id=myvlan0,listen=localhost:8765 -device virtio-net-pci,netdev=myvlan0,mac=52:52:00:14:14:57 -accel whpx,kernel-irqchip=off -sdl -vga std -boot strict=on -display sdl 
 
